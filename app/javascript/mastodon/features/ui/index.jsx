83340578d075f2a321d63d1479cf4e6ce073af11
Aung Kyaw Phyo <kiru.kiru28@gmail.com>
2026-06-17 14:17:21 +0700

    [fork] redirect to /about page

--- app/javascript/mastodon/features/ui/index.jsx
+++ app/javascript/mastodon/features/ui/index.jsx
@@ -148,7 +148,7 @@ class SwitchingColumnsArea extends PureComponent {
     } else if (singleUserMode && owner && initialState?.accounts[owner]) {
       redirect = <Redirect from='/' to={`/@${initialState.accounts[owner].username}`} exact />;
     } else if (trendsEnabled && landingPage === 'trends') {
-      redirect = <Redirect from='/' to='/explore' exact />;
+      redirect = <Redirect from='/' to='/about' exact />;
     } else if (localLiveFeedAccess === 'public' && landingPage === 'local_feed') {
       redirect = <Redirect from='/' to='/public/local' exact />;
     } else {
